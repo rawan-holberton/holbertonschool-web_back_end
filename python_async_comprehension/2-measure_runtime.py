57168950e566572a980a65c5a549c355ac71ca52
Rawan <rawansafouan@gmail.com>
2026-07-02 15:41:03 +0200

    correct pycodestyle

--- python_async_comprehension/2-measure_runtime.py
+++ python_async_comprehension/2-measure_runtime.py
@@ -8,12 +8,13 @@ async_comprehension = __import__("1-async_comprehension").async_comprehension
 
 
 async def measure_runtime() -> float:
-    """Measure total runtime for running async_comprehension 4 times in parallel."""
+    """Measure total runtime for running 
+    async_comprehension 4 times in parallel."""
     start = time.perf_counter()
 
-    await asyncio.gather(async_comprehension(), 
+    await asyncio.gather(async_comprehension(),
                          async_comprehension(),
-                         async_comprehension(), 
+                         async_comprehension(),
                          async_comprehension())
 
     end = time.perf_counter()
